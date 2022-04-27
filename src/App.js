1eca81fe32bbbe1b1f420c16143ad45c8ef31151
AtwijukireBrendah <kire5brenda@gmail.com>
2022-04-27 12:47:24 +0300

    react counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+function App() {
+  const [count,setCount] = useState(0);
+
+    function increaseCount() {
+      setCount(count+1);
+
+    }
+
+    function decreaseCount() {
+      setCount(count-1);
+
+    }
+  function changedValue(e){
+    //get target.value
+    let inputvalue=parseInt(e.target.value);
+    //update state with value
+    setCount(inputvalue);
+    
+  }
+
+
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <input type="number" value={count} onChange={changedValue} />
+        <div className="btn-container">
+          <button onClick={increaseCount}>Increase</button>
+          <button onClick={decreaseCount}>Decrease</button>
+        </div>
+
+      </div>
+    );
+    }
+
+  export default App;
+
+
+  
