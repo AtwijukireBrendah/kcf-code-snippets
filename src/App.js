a2874ee9f95c0308522b5a8e960500fb805023eb
AtwijukireBrendah <kire5brenda@gmail.com>
2022-05-16 11:24:29 +0300

    my first commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,140 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Contact from './components/Contact';
-import About from './components/About';
-import Header from './components/Header';
-import Counter from './components/Counter';
+import React, { useEffect, useState } from "react";
+import {Form, TextField, Button, Icon } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography, CardActions } from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import "./App.css";
+import { SettingsSystemDaydreamRounded } from "@mui/icons-material";
 
-function App(props){
+function App() {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
+  const [repos, setRepos] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [bio, setBio] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(null);
+  const[location,setLocation]=useState("");
+  const[dateJoined,setDateJoined]=useState("");
+ 
+
+  useEffect(() => {
+    let apiUrl = "https://api.github.com/users/atwijukirebrendah";
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+
+  }, [])
+
+
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url,
+    bio,
+    location,
+    created_at,
+    updated_at,
+
+  }) => {
+    setName(name);
+    setUserName(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setBio(bio);
+    setLocation(location);
+    setDateJoined(created_at);
+
+  };
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+    console.log(setUserInput);
+  };
+
+  const handleSubmit = () => {
+    let apiUrl = `https://api.github.com/users/${userInput}`;
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setData(data);
+      })
+  }
+
   return (
-    <BrowserRouter>
-      <Header/>
-      <hr/>
-      <Routes>
-          <Route path="/" element={<h1>Welcome to this site</h1>}/>
-          <Route path="/counter" element={<Counter name="Roland" age="30"/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
-  
+    <div >
+      <div className="navbar">Find Github User Profile</div>
+      <form type="submit" className="form" noValidate autoComplete="on" onSubmit={handleSubmit}>
+        <span id="search">
+          <TextField size="small" id="name" placeholder="Github User" variant="outlined" onChange={handleSearch} />
+        </span>
+        <Button typecontent="Search"
+          variant="contained">Search</Button>
+          </form>
+      <div className="card">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={avatar}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="div">
+              {name}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {userName}
+            </Typography>
+            <hr />
+            <Typography variant="body2" color="text.secondary">
+              {name} is a {bio}
+              <br/>
+              <br/>
+              <a><Icon name="user" />{location}</a>
+              <a><Icon name="user" />Joined {dateJoined}</a>
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <div>
+            <span>
+              <a><Icon name="user" />{repos} Repos</a>
+              <a><Icon name="user" />{followers} Followers</a>
+              <a><Icon name="user" />{following} Following</a>
+            </span>
+            </div>
+            
+           
+            
+          </CardActions>
+        </Card>
+      </div>
+    </div>
+
+  );
+
 }
 
 export default App;
+
+
+
+
+
--- src/components/Contact.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Contact(){
-    return <h1>Please call us via 1929292929</h1>
-}
-
-
-export default Contact;
